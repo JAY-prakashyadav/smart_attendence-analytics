74297d87d306fe1420b3010b9362d06e7b4a6290
JAY-prakashyadav <spyaduvansi423@gmail.com>
2025-09-01 20:52:40 +0530

    Update Attendence.js

--- backend/models/Attendence.js
+++ backend/models/Attendence.js
@@ -1,1 +1,23 @@
+const mongoose = require("mongoose");
+
+const attendanceSchema = new mongoose.Schema({
+  studentId: {
+    type: String,
+    required: true,
+  },
+  classId: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    lat: { type: Number },
+    lng: { type: Number },
+  }
+});
+
+module.exports = mongoose.model("Attendance", attendanceSchema);
 
