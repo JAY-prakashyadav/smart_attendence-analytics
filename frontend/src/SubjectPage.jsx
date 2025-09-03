a9183e739e9daee4a264b5eb86e9ca345758039c
JAY-prakashyadav <spyaduvansi423@gmail.com>
2025-09-03 08:07:37 +0530

    Create SubjectPage.jsx

---
+++ frontend/src/SubjectPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SubjectPage({ subject }) {
+  return (
+    <div>
+      <h1>{subject} Page</h1>
+      <p>Notes, attendance, and other info will appear here.</p>
+    </div>
+  );
+}
+
+export default SubjectPage;
